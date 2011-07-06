e8ba30e6f8052da26d3f3fe94983ad8b0c8995da
vit.jonas@gmail.com <vit.jonas@gmail.com@5e329b0b-a2ee-ea63-e329-299493fc886d>
2011-07-06 07:47:29 +0000

    * added Japanese translation
    git-svn-id: http://redmine-dmsf.googlecode.com/svn/trunk/redmine_dmsf@188 5e329b0b-a2ee-ea63-e329-299493fc886d

---
+++ assets/javascripts/plupload/i18n/ja.js
@@ -0,0 +1,25 @@
+// Japanese
+plupload.addI18n({
+	'Select files' : 'ファイルの選択:',
+	'Add files to the upload queue and click the start button.' : 'アップロードキューにファイルを追加しスタートボタンをクリックしてください。',
+	'Filename' : 'ファイル名',
+	'Status' : '進行状況',
+	'Size' : 'サイズ',
+	'Add files' : 'ファイルの追加',
+	'Stop current upload' : '現在のアップロードを中止',
+	'Start uploading queue' : 'キューのアップロードを開始',
+	'Uploaded %d/%d files': '%d/%d 個のファイルをアップロードしました',
+	'N/A' : 'N/A',
+	'Drag files here.' : 'ここにファイルをドラッグしてください。',
+	'File extension error.': 'ファイル拡張子エラーです。',
+	'File size error.': 'ファイルサイズエラーです。',
+	'Init error.': '初期化エラーです。',
+	'HTTP Error.': 'HTTP エラーです。',
+	'Security error.': 'セキュリティエラーです。',
+	'Generic error.': '原因不明のエラーです。',
+	'IO error.': 'I/O エラーです。',
+	'Stop Upload': 'アップロードの中止',
+	'Add Files': 'ファイルの追加',
+	'Start Upload': 'アップロードの開始',
+	'%d files queued': '%d 個のファイルがキューにあります。'
+});
