7edf6f4c79dd89fba0c1a0e3991468f5e91f2fed
vit.jonas@gmail.com <vit.jonas@gmail.com@5e329b0b-a2ee-ea63-e329-299493fc886d>
2011-07-27 06:54:27 +0000

    * finished Issue 124: Define project_module_dmsf & permission_view_dmsf_folders in .yml for internationalization
    git-svn-id: http://redmine-dmsf.googlecode.com/svn/trunk/redmine_dmsf@209 5e329b0b-a2ee-ea63-e329-299493fc886d

--- init.rb
+++ init.rb
@@ -29,7 +29,7 @@ Redmine::Plugin.register :redmine_dmsf do
   name "DMSF"
   author "Vít Jonáš"
   description "Document Management System Features"
-  version "1.0.0 dev"
+  version "1.0.0"
   url "http://code.google.com/p/redmine-dmsf/"
   author_url "mailto:vit.jonas@gmail.com"
   
