443e497d7495d4cb7cbd8b6b20ad92a3856cafd2
vit.jonas@gmail.com <vit.jonas@gmail.com@5e329b0b-a2ee-ea63-e329-299493fc886d>
2011-09-23 08:14:30 +0000

    * working on Issue 145:	Direct links for sensitive oprations
    git-svn-id: http://redmine-dmsf.googlecode.com/svn/trunk/redmine_dmsf@235 5e329b0b-a2ee-ea63-e329-299493fc886d

--- app/controllers/dmsf_controller.rb
+++ app/controllers/dmsf_controller.rb
@@ -24,6 +24,9 @@ class DmsfController < ApplicationController
   before_filter :find_folder, :except => [:new, :create, :edit_root, :save_root]
   before_filter :find_parent, :only => [:new, :create]
   
+  verify :method => :post, :only => [:delete_entries, :create, :save, :delete, :save_root, :notify_activate, :notify_deactivate], 
+    :render => { :nothing => true, :status => :method_not_allowed }
+  
   def show
     if @folder.nil?
       @subfolders = DmsfFolder.project_root_folders(@project)
