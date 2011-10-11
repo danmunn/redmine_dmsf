0688b7aa67e85addabd43ac4e8debc3ecfb1db80
vit.jonas@gmail.com <vit.jonas@gmail.com@5e329b0b-a2ee-ea63-e329-299493fc886d>
2011-10-11 09:37:56 +0000

    * preparation for 1.2.0 release
    git-svn-id: http://redmine-dmsf.googlecode.com/svn/trunk/redmine_dmsf@256 5e329b0b-a2ee-ea63-e329-299493fc886d

--- init.rb
+++ init.rb
@@ -29,7 +29,7 @@ Redmine::Plugin.register :redmine_dmsf do
   name "DMSF"
   author "Vít Jonáš"
   description "Document Management System Features"
-  version "1.2.0 devel"
+  version "1.2.0"
   url "http://code.google.com/p/redmine-dmsf/"
   author_url "mailto:vit.jonas@gmail.com"
   
