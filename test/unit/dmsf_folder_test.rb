875892495a97d321e887bef428ba2ad63db446ab
Daniel Munn <dan.munn@munnster.co.uk>
2012-06-29 09:44:55 +0100

    Fixed unused test to use our modifed test framework

--- test/unit/dmsf_folder_test.rb
+++ test/unit/dmsf_folder_test.rb
@@ -1,11 +1,11 @@
-require File.dirname(__FILE__) + '/../test_helper'
+require File.expand_path('../../test_helper.rb', __FILE__)
 
-class DmsfFolderTest < ActiveSupport::TestCase
+class DmsfFolderTest < RedmineDmsf::Test::UnitTest
   fixtures :projects, :users, :dmsf_folders, :dmsf_files, :dmsf_file_revisions,
            :roles, :members, :member_roles, :enabled_modules, :enumerations
 
   def test_folder_creating
-    assert_not_nil(dmsf_folders(:one))
+    assert_not_nil(dmsf_folders(:dmsf_folders_001))
   end
   
 end
