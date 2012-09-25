9f9a4081e3c047a3e08f1bbaf71220d01be6cdd3
Daniel Munn <dan.munn@munnster.co.uk>
2012-09-25 22:56:08 +0100

    Additional lock methods

--- lib/dmsf/lockable.rb
+++ lib/dmsf/lockable.rb
@@ -18,9 +18,40 @@ module Dmsf
     end
 
     module InstanceMethods
-      private
-      def all_locks
-        return @locks unless @locks.nil?
+
+      # tree_with_locks
+      # ---------------
+      # Returns ActiveRecord::Relation with all locks included
+      # for hierarchy
+      def tree_with_locks
+        return @tree_with_locks unless @tree_with_locks.nil?
+        @tree_with_locks = self.self_and_ancestors.includes(:locks)
+      end
+
+      # effective_locks
+      # ---------------
+      # Retrieves the hierarchy and
+      def effective_locks
+        return @effective_locks unless @effective_locks.nil?
+        #This forces an inner join which will only return hierarchy items
+        #that have un-expired locks on them
+        hierarchy = tree_with_locks.where('dmsf_locks.expires_at < :now', :now => DateTime.now)
+        @effective_locks = []
+        hierarchy.each do |entity|
+          next if entity.locks.nil? or entity.locks.empty?
+          @effective_locks = entity.locks
+          break
+        end
+        return @effective_locks
+      end
+
+      # locked?
+      # -------
+      # Indicates if the item is locked
+      # (makes use of effective_locks)
+      def locked?
+        #If effective_locks is empty then return false
+        return !effective_locks.empty?
       end
     end
   end
--- test/unit/lib/Dmsf/lockable_test.rb
+++ test/unit/lib/Dmsf/lockable_test.rb
@@ -28,9 +28,67 @@ module Dmsf
       end
 
       context 'InstanceMethods' do
+        setup do
+          return_item = Dmsf::File.new
+          return_item.stubs(:locks).returns([Dmsf::Lock.new])
+          @item = return_item
+          @item.stubs(:tree_with_locks).returns([return_item])
+        end
+        context 'Method tree_with_locks' do
+          should 'Get contents of hierarchy' do
+            @item.unstub(:tree_with_locks)
+            expectation = mock()
+            expectation.expects(:includes).with(:locks).returns([@item])
+            @item.expects(:self_and_ancestors).returns(expectation)
+            @item.tree_with_locks
+          end
+        end
+
+        context 'Method effective_locks' do
+          should 'call tree_with_locks with additional where clause' do
+            #We need to mock not stub this
+            @item.unstub(:tree_with_locks)
+            expectation = mock()
+            expectation.expects(:where).returns([])
+            @item.expects(:tree_with_locks).returns(expectation)
+            @item.effective_locks
+          end
+
+          should "return an array of locks at lowest effective point in hierarchy" do
+            @item.unstub(:tree_with_locks)
+            expectation = mock()
+            expectation.stubs(:where).returns([@item])
+            @item.stubs(:tree_with_locks).returns(expectation)
+            value = @item.effective_locks
+            assert value.is_a?(Array)
+            assert !value.empty?
+          end
+
+          should "return an empty array when no locks exist on item" do
+            @item.unstub(:tree_with_locks)
+            expectation = mock()
+            expectation.stubs(:where).returns([])
+            @item.stubs(:tree_with_locks).returns(expectation)
+            value = @item.effective_locks
+            assert value.is_a?(Array)
+            assert value.empty?
+          end
+
+        end
 
         context 'Method locked?' do
-          should 'utilise protected method '
+          should 'utilise method effective_locks' do
+            @item.expects(:effective_locks).returns([])
+            @item.locked?
+          end
+          should 'return true when valid lock exists on item' do
+            @item.stubs(:effective_locks).returns([Dmsf::Lock.new])
+            assert @item.locked?
+          end
+          should 'return false when no locks are available to item' do
+            @item.stubs(:effective_locks).returns([])
+            assert !@item.locked?
+          end
         end
       end
 
