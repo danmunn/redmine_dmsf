cfbf33eb13ef54ac45081d4f9a60db9e2608d736
Daniel Munn <dan.munn@munnster.co.uk>
2012-09-26 14:06:34 +0100

    Lockable effective_locks updated to use arel Lockable unlock! introduced

--- lib/dmsf/lockable.rb
+++ lib/dmsf/lockable.rb
@@ -1,5 +1,12 @@
+
 module Dmsf
   module Lockable
+
+    class ResourceLocked < IOError; end
+    class ResourceParentLocked < IOError; end
+    class ResourceNotLocked < RuntimeError; end
+    class LockNotOwnedByPrincipal < SecurityError; end
+
     def self.included(base)
       #Extend Static (Class) Methods
       base.extend(ClassMethods)
@@ -34,8 +41,14 @@ module Dmsf
       def effective_locks
         return @effective_locks unless @effective_locks.nil?
         #This forces an inner join which will only return hierarchy items
-        #that have un-expired locks on them
-        hierarchy = tree_with_locks.where('dmsf_locks.expires_at < :now', :now => DateTime.now)
+        #that have un-expired locks on them (or locks without expiry)
+
+        #Note: we use arel to build the condition
+        expires_arel = Dmsf::Lock.arel_table[:expires_at]
+        hierarchy = tree_with_locks.where(
+          expires_arel.lt(DateTime.now).
+          or(expires_arel.eq(nil))
+        )
         @effective_locks = []
         hierarchy.each do |entity|
           next if entity.locks.nil? or entity.locks.empty?
@@ -52,6 +65,18 @@ module Dmsf
       def locked?
         #If effective_locks is empty then return false
         return !effective_locks.empty?
+      end
+
+      # unlock!
+      # -------
+      # If resource is locked, deletes lock
+      # otherwise raises an exception
+      def unlock! (user = nil)
+        user ||= User.current
+        raise Dmsf::Lockable::ResourceNotLocked unless locked?
+        raise Dmsf::Lockable::ResourceParentLocked unless effective_locks.first.entity_id == id
+        raise Dmsf::Lockable::LockNotOwnedByPrincipal unless user.id == effective_locks.first.user_id
+        effective_locks.first.delete
       end
     end
   end
--- test/unit/lib/dmsf__lockable_test.rb
+++ test/unit/lib/dmsf__lockable_test.rb
@@ -1,7 +1,27 @@
-require File.dirname(__FILE__) + '/../../../test_helper'
+require File.dirname(__FILE__) + '/../../test_helper'
 module Dmsf
   class LockableTest < Test::UnitTest
     context "Dmsf::Lockable" do
+      context "Exception type ResourceLocked" do
+        should "Inherit IOError" do
+          assert_equal Dmsf::Lockable::ResourceLocked.superclass, IOError
+        end
+      end
+      context "Exception type ResourceParentLocked" do
+        should "Inherit IOError" do
+          assert_equal Dmsf::Lockable::ResourceParentLocked.superclass, IOError
+        end
+      end
+      context "Exception type ResourceNotLocked" do
+        should "Inherit RuntimeError" do
+          assert_equal Dmsf::Lockable::ResourceNotLocked.superclass, RuntimeError
+        end
+      end
+      context "Exception type LockNotOwnedByPrincipal" do
+        should "Inherit SecurityError" do
+          assert_equal Dmsf::Lockable::LockNotOwnedByPrincipal.superclass, SecurityError
+        end
+      end
       context "Static method included" do
         setup do
           @m_class = stub_everything('Dmsf::Lockable.included')
@@ -89,6 +109,45 @@ module Dmsf
             @item.stubs(:effective_locks).returns([])
             assert !@item.locked?
           end
+        end
+
+        context 'Method unlock!' do
+#          setup do
+#            @item.unstub(:tree_with_locks)
+#            expectation = mock()
+#            expectation.stubs(:where).returns([@item])
+#            @item.stubs(:tree_with_locks).returns(expectation)
+#          end
+          should 'raise ResourceNotLocked when not locked' do
+            assert_raise(Dmsf::Lockable::ResourceNotLocked) do
+              @item.stubs(:locked?).returns(false)
+              @item.unlock!
+            end
+          end
+
+          should 'raise ResourceParentLocked when item in hierarchy above principal is locked' do
+            assert_raise(Dmsf::Lockable::ResourceParentLocked) do
+              @item.stubs(:effective_locks).returns([Dmsf::Lock.new(:entity_id => 1)])
+              @item.unlock!
+            end
+          end
+
+          should 'raise LockNotOwnedByPrincipal when called with different id to owner' do
+            assert_raise(Dmsf::Lockable::LockNotOwnedByPrincipal) do
+              @item.stubs(:effective_locks).returns([Dmsf::Lock.new()])
+              @item.unlock!() #Lock does not hold an id
+            end
+          end
+
+          should 'delete the lock when all criteria are met' do
+            expectation = mock()
+            expectation.expects(:delete).returns(true)
+            expectation.stubs(:entity_id).returns(@item.id)
+            expectation.stubs(:user_id).returns(1)
+            @item.stubs(:effective_locks).returns([expectation])
+            @item.unlock!(User.find(1)) #Lock does not hold an id
+          end
+
         end
       end
 
