3984704005db038f29a119a5eea330679154bac1
Karel Picman <karel.picman@kontron.com>
2012-11-02 13:58:02 +0100

    Wrong attribute index for file id => full-text searching doesn't work

--- app/models/dmsf_file.rb
+++ app/models/dmsf_file.rb
@@ -357,7 +357,7 @@ class DmsfFile < ActiveRecord::Base
               next if dmsf_attrs[1].blank?
               next unless results.select{|f| f.id.to_s == dmsf_attrs[1]}.empty?
               
-              dmsf_file = DmsfFile.where(limit_options[:conditions]).where(:id => dmsf_attrs[1], :deleted => false).first
+              dmsf_file = DmsfFile.where(limit_options[:conditions]).where(:id => dmsf_attrs[2], :deleted => false).first
     
               if !dmsf_file.nil?
                 if options[:offset]
