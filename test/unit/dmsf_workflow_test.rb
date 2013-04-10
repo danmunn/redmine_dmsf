3f4100fe680991014c534cbaebd7f802db98ed09
Karel Picman <karel.picman@kontron.com>
2013-04-10 15:41:49 +0200

    Workflow models and controller tests created

--- test/unit/dmsf_workflow_test.rb
+++ test/unit/dmsf_workflow_test.rb
@@ -2,8 +2,48 @@ require File.expand_path('../../test_helper', __FILE__)
 
 class DmsfWorkflowTest < ActiveSupport::TestCase
 
-  # Replace this with your real tests.
+  def setup
+    @wf1 = Dmsf::Workflow.find(1)
+    @wf2 = Dmsf::Workflow.find(2)
+  end
+  
   def test_truth
-    assert true
+    assert_kind_of Dmsf::Workflow, @wf1
+  end
+  
+  def test_create
+    workflow = Dmsf::Workflow.new(:name => 'wf')   
+    assert workflow.save
+  end
+  
+  def test_update
+    assert_equal 'wf1', @wf1.name
+    @wf1.name = 'wf1a'
+    assert @wf1.save
+    @wf1.reload
+    assert_equal 'wf1a', @wf1.name
+  end
+  
+  def test_validate_name_length
+    @wf1.name = 'a' * 256
+    assert !@wf1.save
+    assert_equal 1, @wf1.errors.count        
+  end
+  
+  def test_validate_name_presence
+    @wf1.name = ''
+    assert !@wf1.save
+    assert_equal 1, @wf1.errors.count        
+  end
+  
+  def test_validate_name_uniqueness
+    @wf2.name = @wf1.name
+    assert !@wf2.save
+    assert_equal 1, @wf2.errors.count        
+  end
+  
+  def test_destroy  
+    @wf2.destroy
+    assert_nil Dmsf::Workflow.find_by_id(2)
   end
 end
