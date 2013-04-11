2f19116983e8ed4d8dde8f8ca330f88cf088f083
Daniel Munn <dan.munn@munnster.co.uk>
2013-04-11 06:52:41 +0100

    Updating some styling for viewing dmsf folder

--- app/helpers/dmsf_helper.rb
+++ app/helpers/dmsf_helper.rb
@@ -11,4 +11,27 @@ module DmsfHelper
   def render_parent_link(parent)
     return ''
   end
+
+  def render_dmsf_entity_list
+  end
+
+  def dmsf_stylesheet_link_tags(args = nil)
+    args ||= {}
+    default_args = {:controller => true, :global => false}
+    args = default_args.merge(args)
+    _return = ''
+
+    if args[:global]
+      _return << stylesheet_link_tag('dmsf', :media => "all", :plugin => "redmine_dmsf")
+    end
+
+    if !params[:controller].blank? && args[:controller]
+      begin
+        _return << stylesheet_link_tag('dmsf_' + params[:controller].downcase, :media => "all", :plugin => "redmine_dmsf")
+      rescue Exception => e
+        e.message
+      end
+    end
+    return _return.html_safe
+  end
 end
