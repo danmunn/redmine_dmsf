cba717b9aef9393aabf03c5476441d6e3a219593
Karel Picman <karel.picman@kontron.com>
2013-11-20 08:30:28 +0100

    #176 - 500 internal error when approving workflow - dmsf_workflows/4/new_action

--- app/controllers/dmsf_workflows_controller.rb
+++ app/controllers/dmsf_workflows_controller.rb
@@ -104,7 +104,7 @@ class DmsfWorkflowsController < ApplicationController
                         assignment.user, 
                         @workflow, 
                         revision,
-                        l(:text_email_subject_reequires_approval, :name => @workflow.name),                        
+                        l(:text_email_subject_requires_approval, :name => @workflow.name),                        
                         l(:text_email_finished_step, :name => @workflow.name, :filename => revision.file.name),
                         l(:text_email_to_proceed)).deliver
                     end                    
