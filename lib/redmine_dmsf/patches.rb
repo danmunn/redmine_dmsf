96fbb8ae6664aca8fbfc1ecd045380c1f2cbe537
Karel Picman <karel.picman@kontron.com>
2014-02-20 13:45:38 +0100

    undefined method CustomFieldsHelper'

--- lib/redmine_dmsf/patches.rb
+++ lib/redmine_dmsf/patches.rb
@@ -1,7 +1,8 @@
 # Redmine plugin for Document Management System "Features"
 #
-# Copyright (C) 2011   Vít Jonáš <vit.jonas@gmail.com>
-# Copyright (C) 2012   Daniel Munn <dan.munn@munnster.co.uk>
+# Copyright (C) 2011    Vít Jonáš <vit.jonas@gmail.com>
+# Copyright (C) 2012    Daniel Munn <dan.munn@munnster.co.uk>
+# Copyright (C) 2011-14 Karel Picman <karel.picman@kontron.com>
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -17,7 +18,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 # 
-require 'redmine_dmsf/patches/custom_fields_helper'
+require 'redmine_dmsf/patches/custom_fields_helper_patch'
 require 'redmine_dmsf/patches/acts_as_customizable'
 require 'redmine_dmsf/patches/project_patch'
 require 'redmine_dmsf/patches/project_tabs_extended'
--- lib/redmine_dmsf/patches/custom_fields_helper_patch.rb
+++ lib/redmine_dmsf/patches/custom_fields_helper_patch.rb
@@ -1,8 +1,8 @@
 # Redmine plugin for Document Management System "Features"
 #
-# Copyright (C) 2011   Vít Jonáš <vit.jonas@gmail.com>
-# Copyright (C) 2012   Daniel Munn <dan.munn@munnster.co.uk>
-# Copyright (C) 2013   Karel Pičman <karel.picman@kontron.com>
+# Copyright (C) 2011    Vít Jonáš <vit.jonas@gmail.com>
+# Copyright (C) 2012    Daniel Munn <dan.munn@munnster.co.uk>
+# Copyright (C) 2011-14 Karel Picman <karel.picman@kontron.com>
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -20,7 +20,7 @@
 
 module RedmineDmsf
   module Patches
-    module CustomFieldsHelper
+    module CustomFieldsHelperPatch
       def self.included(base)
         base.class_eval do
           alias_method_chain :custom_fields_tabs, :customer_tab
@@ -40,7 +40,7 @@ end
 
 # Apply patch
 Rails.configuration.to_prepare do
-  unless CustomFieldsHelper.included_modules.include?(CustomFieldsHelper)
-    CustomFieldsHelper.send(:include, RedmineDmsf::Patches::CustomFieldsHelper)
+  unless CustomFieldsHelper.included_modules.include?(RedmineDmsf::Patches::CustomFieldsHelperPatch)
+    CustomFieldsHelper.send(:include, RedmineDmsf::Patches::CustomFieldsHelperPatch)
   end
 end
