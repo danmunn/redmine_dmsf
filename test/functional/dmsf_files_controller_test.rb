c2a40de2bafc5afacefd60bc3c9852a55bacc8ce
Karel Picman <karel.picman@kontron.com>
2014-03-11 13:58:34 +0100

    #233 Failed Travis builds

--- test/functional/dmsf_files_controller_test.rb
+++ test/functional/dmsf_files_controller_test.rb
@@ -21,35 +21,49 @@ require File.expand_path('../../test_helper', __FILE__)
 class DmsfFilesControllerTest < RedmineDmsf::Test::TestCase 
   
   fixtures :users, :dmsf_files, :dmsf_file_revisions, :custom_fields, 
-    :custom_values, :projects, :members, :member_roles, :enabled_modules,
+    :custom_values, :projects, :roles, :members, :member_roles, :enabled_modules,
     :dmsf_file_revisions
 
   def setup
-    @request.session[:user_id] = 2    
-    @file = DmsfFolder.find_by_id 1    
+    @project = Project.find_by_id 1
+    assert_not_nil @project
+    @project.enable_module! :dmsf
+    @user = User.find_by_id 2
+    assert_not_nil @user
+    @request.session[:user_id] = @user.id
+    @file = DmsfFile.find_by_id 1    
     @role = Role.find_by_id 1
     @custom_field = CustomField.find_by_id 21
-    @custom_value_2 = CustomValue.find_by_id 22
+    @custom_value = CustomValue.find_by_id 22
   end 
+  
+  def test_truth
+    assert_kind_of Project, @project
+    assert_kind_of User, @user
+    assert_kind_of DmsfFile, @file
+    assert_kind_of Role, @role
+    assert_kind_of CustomField, @custom_field
+    assert_kind_of CustomValue, @custom_value
+  end
     
   def test_show_file
     # Missing permissions
-    get :show, :id => @file
+    get :show, :id => @file.id
     assert_response 403    
     
     # Permissions OK
     @role.add_permission! :view_dmsf_files
     @role.add_permission! :file_manipulation
-    get :show, :id => @file    
+    get :show, :id => @file.id 
     assert_response :success
     
     # The last revision
-    assert_select 'label', {:text => @custom_field.name}
-    assert_select '.customfield', {:text => "#{@custom_field.name}: #{@custom_value_2.value}"}
+    assert_select 'label', { :text => @custom_field.name }
+    assert_select '.customfield', { :text => "#{@custom_field.name}: #{@custom_value.value}" }
     
     # A new revision
-    assert_select 'label', {:text => @custom_field.name}
-    assert_select 'option', {:value => @custom_value_2.value}
+    assert_select 'label', { :text => @custom_field.name }
+    assert_select 'option', { :value => @custom_value.value }
   end
 end
 
