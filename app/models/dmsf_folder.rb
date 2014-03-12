31be631a92a525b1783922e35c911849ca47108d
Karel Picman <karel.picman@kontron.com>
2014-03-12 07:13:42 +0100

    Wrong a folder files count in the main view

--- app/models/dmsf_folder.rb
+++ app/models/dmsf_folder.rb
@@ -1,6 +1,7 @@
 # Redmine plugin for Document Management System "Features"
 #
-# Copyright (C) 2011   Vít Jonáš <vit.jonas@gmail.com>
+# Copyright (C) 2011    Vít Jonáš <vit.jonas@gmail.com>
+# Copyright (C) 2011-14 Karel Pičman <karel.picman@konton.com>
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
