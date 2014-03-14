ef3df86a1000ebc345bb9d93f6361fb94cb42369
Karel Picman <karel.picman@kontron.com>
2014-03-14 10:57:42 +0100

    #235 'You are not member of the project' when changing project notification.

---
+++ test/functional/dmsf_state_controller_test.rb
@@ -0,0 +1,65 @@
+# Redmine plugin for Document Management System "Features"
+#
+# Copyright (C) 2011-14 Karel Pičman <karel.picman@kontron.com>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+require File.expand_path('../../test_helper', __FILE__)
+
+class DmsfStateControllerTest < RedmineDmsf::Test::TestCase
+  include Redmine::I18n  
+    
+  fixtures :users, :projects, :members, :roles, :member_roles
+
+  def setup
+    @user_admin = User.find_by_id 1 # Redmine admin
+    @user_member = User.find_by_id 2 # John Smith - manager
+    @user_non_member = User.find_by_id 3 # Dave Lopper    
+    @project = Project.find_by_id 1
+    assert_not_nil @project
+    @project.enable_module! :dmsf
+    @role_manager = Role.find_by_name('Manager')
+    @role_manager.add_permission! :user_preferences
+  end
+  
+  def test_truth
+    assert_kind_of User, @user_admin
+    assert_kind_of User, @user_member
+    assert_kind_of User, @user_non_member
+    assert_kind_of Project, @project        
+    assert_kind_of Role, @role_manager
+  end
+    
+  def test_user_pref_save
+    # Member
+    @request.session[:user_id] = @user_member.id
+    post :user_pref_save, :id => @project.id, :email_notify => 1
+    assert_redirected_to settings_project_path(@project, :tab => 'dmsf')        
+    assert_not_nil flash[:notice]
+    assert_equal flash[:notice], l(:notice_your_preferences_were_saved)
+    
+    # Non Member
+    @request.session[:user_id] = @user_non_member.id    
+    post :user_pref_save, :id => @project.id, :email_notify => 1
+    assert_response :forbidden
+    
+    # Admin - non member
+    @request.session[:user_id] = @user_admin.id
+    post :user_pref_save, :id => @project.id, :email_notify => 1
+    assert_redirected_to settings_project_path(@project, :tab => 'dmsf')        
+    assert_not_nil flash[:warning]
+    assert_equal flash[:warning], l(:user_is_not_project_member)
+  end
+end
