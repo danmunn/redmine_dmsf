a1cb1e23137ea6fb372b78a9801c1dbcbae3263d
Karel Pičman <karel.picman@kontron.com>
2014-09-18 13:45:44 +0200

    ubuntu migrate failed #288

--- app/models/dmsf_lock.rb
+++ app/models/dmsf_lock.rb
@@ -1,7 +1,8 @@
 # Redmine plugin for Document Management System "Features"
 #
-# Copyright (C) 2011   Vít Jonáš <vit.jonas@gmail.com>
-# Copyright (C) 2012   Daniel Munn  <dan.munn@munnster.co.uk>
+# Copyright (C) 2011    Vít Jonáš <vit.jonas@gmail.com>
+# Copyright (C) 2012    Daniel Munn  <dan.munn@munnster.co.uk>
+# Copyright (C) 2011-14 Karel Picman <karel.picman@kontorn.com>
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -16,6 +17,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+require 'simple_enum'
 
 class DmsfLock < ActiveRecord::Base
   before_create :generate_uuid
