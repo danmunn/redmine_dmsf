ef4df8d9b8fbe5d7f036062b496f9898dcb3c0ca
Karel Pičman <karel.picman@kontron.com>
2015-02-02 08:50:46 +0100

    Add an External Link type #342

--- db/migrate/20150130052716_add_external.rb
+++ db/migrate/20150130052716_add_external.rb
@@ -2,14 +2,12 @@ class AddExternal < ActiveRecord::Migration
   def up
     change_column :dmsf_links, :target_id, :integer, :null => true
 
-    add_column :dmsf_links, :external_url, :string, :null => true
-    add_column :dmsf_links, :user_id, :integer
+    add_column :dmsf_links, :external_url, :string, :null => true    
   end
 
   def down
     change_column :dmsf_links, :target_id, :integer, :null => false
 
-    remove_column :dmsf_links, :external_url
-    remove_column :dmsf_links, :user_id
+    remove_column :dmsf_links, :external_url    
   end
 end
