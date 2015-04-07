7dd4f3f79ed86f6e4c278461b0d660cbba1ba119
Karel Pičman <karel.picman@kontron.com>
2015-04-07 08:42:20 +0200

    Cannot open files in utf-8 folders via webdav #299

--- lib/redmine_dmsf/webdav/base_resource.rb
+++ lib/redmine_dmsf/webdav/base_resource.rb
@@ -95,7 +95,7 @@ module RedmineDmsf
 
       def parent
         p = @__proxy.parent
-        return nil unless p
+        return nil if p.nil? 
         return p.resource.nil? ? p : p.resource
       end
 
