8d58bbda2de44f079785da2574ce5052c6c74c35
Karel Picman <karel.picman@kontron.com>
2016-04-12 14:31:27 +0200

    Bug with "delegate approval step" #523

--- app/helpers/dmsf_workflows_helper.rb
+++ app/helpers/dmsf_workflows_helper.rb
@@ -1,5 +1,5 @@
 # encoding: utf-8
-# 
+#
 # Redmine plugin for Document Management System "Features"
 #
 # Copyright (C) 2011-16 Karel Pičman <karel.picman@kontron.com>
@@ -19,31 +19,33 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 module DmsfWorkflowsHelper
-  
+
   def render_principals_for_new_dmsf_workflow_users(workflow, dmsf_workflow_step_assignment_id, dmsf_file_revision_id)
     scope = workflow.delegates(params[:q], dmsf_workflow_step_assignment_id, dmsf_file_revision_id)
     principal_count = scope.count
     principal_pages = Redmine::Pagination::Paginator.new principal_count, 10, params['page']
     principals = scope.offset(principal_pages.offset).limit(principal_pages.per_page).all
-    
-    if dmsf_workflow_step_assignment_id      
+
+    # Delegation
+    if dmsf_workflow_step_assignment_id
       s = content_tag('div',
-        content_tag('div', principals_radio_button_tags('user_ids[]', principals), :id => 'users_for_delegate'),
+        content_tag('div', principals_radio_button_tags('step_action', principals), :id => 'users_for_delegate'),
         :class => 'objects-selection')
-    else      
+    # New step
+    else
       s = content_tag('div',
         content_tag('div', principals_check_box_tags('user_ids[]', principals), :id => 'users'),
         :class => 'objects-selection')
     end
 
     links = pagination_links_full(principal_pages, principal_count, :per_page_links => false) {|text, parameters, options|
-      link_to text, autocomplete_for_user_dmsf_workflow_path(workflow, parameters.merge(:q => params[:q], :format => 'js')), :remote => true      
+      link_to text, autocomplete_for_user_dmsf_workflow_path(workflow, parameters.merge(:q => params[:q], :format => 'js')), :remote => true
     }
 
     s + content_tag('p', links, :class => 'pagination')
-    s.html_safe           
+    s.html_safe
   end
-  
+
   def dmsf_workflow_steps_options_for_select(steps)
     options = Array.new
     options << [l(:dmsf_new_step), 0]
@@ -51,10 +53,10 @@ module DmsfWorkflowsHelper
       options << [step.to_s, step]
     end
     options_for_select(options, 0)
-  end   
-  
+  end
+
   def dmsf_workflows_for_select(project, dmsf_workflow_id)
-    options = Array.new        
+    options = Array.new
     options << ['', -1]
     DmsfWorkflow.active.sorted.where(['project_id = ? OR project_id IS NULL', project.id]).each do |wf|
       if wf.project_id
@@ -62,14 +64,14 @@ module DmsfWorkflowsHelper
       else
         options << ["#{wf.name} (global)", wf.id]
       end
-    end        
+    end
     options_for_select(options, :selected => dmsf_workflow_id)
-  end  
-  
+  end
+
   def dmsf_all_workflows_for_select(dmsf_workflow_id)
-    options = Array.new        
-    options << ['', 0]    
-    DmsfWorkflow.active.sorted.all.each do |wf|            
+    options = Array.new
+    options << ['', 0]
+    DmsfWorkflow.active.sorted.all.each do |wf|
       if wf.project_id
         prj = Project.find_by_id wf.project_id
         if User.current.allowed_to?(:manage_workflows, prj)
@@ -83,33 +85,33 @@ module DmsfWorkflowsHelper
       else
         # Global approval workflows
         options << [wf.name, wf.id]
-      end      
-    end            
+      end
+    end
     options_for_select(options, :selected => dmsf_workflow_id)
-  end  
-  
+  end
+
   def principals_radio_button_tags(name, principals)
     s = ''
     principals.each do |principal|
       s << "<label>#{ radio_button_tag name, principal.id * 10, false, :id => nil } #{h principal}</label>\n"
     end
-    s.html_safe    
+    s.html_safe
   end
-  
-  def change_status_link(workflow)  
+
+  def change_status_link(workflow)
     url = { :controller => 'dmsf_workflows', :action => 'update', :id => workflow.id }
     if workflow.locked?
-      link_to l(:button_unlock), url.merge(:dmsf_workflow => {:status => DmsfWorkflow::STATUS_ACTIVE}), :method => :put, :class => 'icon icon-unlock'          
+      link_to l(:button_unlock), url.merge(:dmsf_workflow => {:status => DmsfWorkflow::STATUS_ACTIVE}), :method => :put, :class => 'icon icon-unlock'
     else
-      link_to l(:button_lock), url.merge(:dmsf_workflow => {:status => DmsfWorkflow::STATUS_LOCKED}), :method => :put, :class => 'icon icon-lock'      
+      link_to l(:button_lock), url.merge(:dmsf_workflow => {:status => DmsfWorkflow::STATUS_LOCKED}), :method => :put, :class => 'icon icon-lock'
     end
   end
-  
+
   def workflows_status_options_for_select(selected)
     worflows_count_by_status = DmsfWorkflow.global.group('status').count.to_hash
     options_for_select([[l(:label_all), ''],
-      ["#{l(:status_active)} (#{worflows_count_by_status[DmsfWorkflow::STATUS_ACTIVE].to_i})", DmsfWorkflow::STATUS_ACTIVE.to_s],                        
+      ["#{l(:status_active)} (#{worflows_count_by_status[DmsfWorkflow::STATUS_ACTIVE].to_i})", DmsfWorkflow::STATUS_ACTIVE.to_s],
       ["#{l(:status_locked)} (#{worflows_count_by_status[DmsfWorkflow::STATUS_LOCKED].to_i})", DmsfWorkflow::STATUS_LOCKED.to_s]], selected.to_s)
   end
-  
+
 end
