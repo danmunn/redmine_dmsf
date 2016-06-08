9e937525afd50b7334bf0eeea75e463b61cd9dbc
Karel Picman <karel.picman@kontron.com>
2016-06-08 16:55:04 +0200

    Cannot download folders with sub folders #530

--- lib/dmsf_zip.rb
+++ lib/dmsf_zip.rb
@@ -64,7 +64,7 @@ class DmsfZip
       string_path = string_path[(root_path.length + 1) .. string_path.length] if root_path
       @zip_file.put_next_entry(string_path)
       @folders << folder
-      folder.dmsf_folders.visible.each { |subfolder| self.add_folder(subfolder, root_path) }
+      folder.dmsf_folders.visible.each { |subfolder| self.add_folder(subfolder, member, root_path) }
       folder.dmsf_files.visible.each { |file| self.add_file(file, member, root_path) }
     end
   end
