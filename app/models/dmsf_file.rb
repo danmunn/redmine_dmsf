8a44944d6ab3fdb696c160213fa647889e9dd61c
Karel Picman <karel.picman@kontron.com>
2017-06-05 13:09:30 +0200

    Make the storage path Rails.root related #733

--- app/models/dmsf_file.rb
+++ app/models/dmsf_file.rb
@@ -103,7 +103,7 @@ class DmsfFile < ActiveRecord::Base
     return @@storage_path if @@storage_path.present?
     path = Setting.plugin_redmine_dmsf['dmsf_storage_directory']
     if path.blank?
-      path = 'dmsf'
+      path = Pathname.new('files').join('dmsf').to_s
     else
       pn = Pathname.new(path)
       return pn if pn.absolute?
