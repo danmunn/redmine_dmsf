9d1a202a7d71192331745289fb917dde5d5014c5
Karel Picman <karel.picman@kontron.com>
2017-08-31 13:19:23 +0200

    Ruby >= 2.2.2 required

--- after_init.rb
+++ after_init.rb
@@ -69,7 +69,7 @@ else
   end
 end
 
-ActionDispatch::Reloader.to_prepare do
+def prepare
   # Rubyzip configuration
   Zip.unicode_names = true
 
@@ -80,4 +80,14 @@ ActionDispatch::Reloader.to_prepare do
 
   Redmine::Activity.register :dmsf_file_revision_accesses, :default => false
   Redmine::Activity.register :dmsf_file_revisions
+end
+
+if Rails::VERSION::MAJOR >= 5 && Rails::VERSION::MINOR >= 1
+  ActiveSupport::Reloader.to_prepare do
+    prepare
+  end
+else
+  ActionDispatch::Reloader.to_prepare do
+    prepare
+  end
 end
