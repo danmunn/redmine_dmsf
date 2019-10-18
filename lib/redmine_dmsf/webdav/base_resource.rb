7488bf9010e6892d034cb86e85580944a394140c
Karel Pičman <karel.picman@kontron.com>
2019-10-18 10:57:44 +0200

    Accessible even if WebDAV is disabled. #1052

--- lib/redmine_dmsf/webdav/base_resource.rb
+++ lib/redmine_dmsf/webdav/base_resource.rb
@@ -30,6 +30,7 @@ module RedmineDmsf
       attr_reader :public_path
 
       def initialize(path, request, response, options)
+        raise NotFound if Setting.plugin_redmine_dmsf['dmsf_webdav'].blank?
         @project = nil
         @public_path = "#{options[:root_uri_path]}#{path}"
         super(path, request, response, options)
