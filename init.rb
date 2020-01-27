7241a0dc01ea7b2a6dd8cdac1ed2b8e568a060f5
Karel Pičman <karel.picman@kontron.com>
2020-01-27 10:10:18 +0100

    Wrong plugin's version

--- init.rb
+++ init.rb
@@ -33,7 +33,7 @@ Redmine::Plugin.register :redmine_dmsf do
   end
   author 'Vít Jonáš / Daniel Munn / Karel Pičman'
   description 'Document Management System Features'
-  version '2.4.2 devel'
+  version '2.4.2'
 
   requires_redmine version_or_higher: '4.1.0'
 
