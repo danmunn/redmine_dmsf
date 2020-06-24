1c220b7e66a8882fe7f74a1e3ae994b938a2bb84
karel.picman@lbcfree.net <karel.picman@lbcfree.net>
2020-06-24 10:46:28 +0200

    New Error 500 when attempting to modify a document #1140

--- app/models/dmsf_file_revision_access_query.rb
+++ app/models/dmsf_file_revision_access_query.rb
@@ -73,8 +73,6 @@ class DmsfFileRevisionAccessQuery < Query
     @available_columns
   end
 
-  #alias default_columns_names available_columns
-
   ######################################################################################################################
   # New
 
@@ -82,7 +80,7 @@ class DmsfFileRevisionAccessQuery < Query
     base_scope.
         access_grouped.
         joins(:user).
-        order('`count` DESC').
+        order(Arel.sql('COUNT(*) DESC')).
         limit(options[:limit]).
         offset(options[:offset])
   end
