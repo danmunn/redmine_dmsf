0dbb00e8c909d40cd7b693277945e784b10593ad
karel.picman@lbcfree.net <karel.picman@lbcfree.net>
2020-10-19 09:10:06 +0200

    #1179 better fragments checking

--- lib/redmine_dmsf/webdav/dmsf_resource.rb
+++ lib/redmine_dmsf/webdav/dmsf_resource.rb
@@ -176,7 +176,7 @@ module RedmineDmsf
         elsif folder
           # To fullfil Litmus requirements to not delete folder if fragments are in the URL
           uri = URI(request.get_header('REQUEST_URI'))
-          raise BadRequest if uri.fragment
+          raise BadRequest if uri.fragment.present?
           raise Forbidden unless User.current.admin? || User.current.allowed_to?(:folder_manipulation, project)
           raise Forbidden unless DmsfFolder.permissions?(folder, false)
           folder.delete(false) ? NoContent : Conflict
