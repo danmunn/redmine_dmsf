b4c94e56c47b8502e47e46050f41874993bcc348
karel.picman@lbcfree.net <karel.picman@lbcfree.net>
2021-06-11 08:04:20 +0200

    500 Error when doing Approval Workflow related actions #1260

--- app/models/dmsf_workflow.rb
+++ app/models/dmsf_workflow.rb
@@ -223,8 +223,7 @@ class DmsfWorkflow < ActiveRecord::Base
         assignments.first&.dmsf_workflow_step)
     if Setting.plugin_redmine_dmsf['dmsf_display_notified_recipients'] && controller
       unless recipients.blank?
-        to = recipients.collect{ |r| controller.__send__(:h, r.name) }.first(DMSF_MAX_NOTIFICATION_RECEIVERS_INFO).join(
-          ', ')
+        to = recipients.collect{ |r| r.name }.first(DMSF_MAX_NOTIFICATION_RECEIVERS_INFO).join(', ')
         to << ((recipients.count > DMSF_MAX_NOTIFICATION_RECEIVERS_INFO) ? ',...' : '.')
         controller.flash[:warning] = l(:warning_email_notifications, to: to)
       end
