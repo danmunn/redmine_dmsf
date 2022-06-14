81abe8d98106851814c16caa6b013c79a26ad275
Karel Pičman <karel.picman@kontron.com>
2022-06-14 08:33:39 +0200

    warning: already initialized constant Dav4rack::Utils::DEFAULT_HTTP_VERSION

--- lib/redmine_dmsf/webdav/custom_middleware.rb
+++ lib/redmine_dmsf/webdav/custom_middleware.rb
@@ -19,8 +19,6 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-require File.dirname(__FILE__) + '/../../dav4rack'
-
 module RedmineDmsf
   module Webdav
 
