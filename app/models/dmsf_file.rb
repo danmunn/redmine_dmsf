f08a9f1f67683a74b9300fc024c6fa42d3a89e79
Karel Pičman <karel.picman@kontron.com>
2023-06-02 12:43:13 +0200

    I downloaded files from devel branch and put them into plugin folder. Then I run #1453

--- app/models/dmsf_file.rb
+++ app/models/dmsf_file.rb
@@ -20,6 +20,7 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 require 'xapian'
+require "#{File.dirname(__FILE__)}/../../lib/redmine_dmsf/lockable"
 
 # File
 class DmsfFile < ApplicationRecord
