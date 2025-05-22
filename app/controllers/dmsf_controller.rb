b9d31a40709a1db19d66cfdd0e5f999b4eed4125
1redmine <noreply@kontron.com>
2025-05-22 12:42:27 +0200

    #1596 Rubocop

--- app/controllers/dmsf_controller.rb
+++ app/controllers/dmsf_controller.rb
@@ -95,7 +95,7 @@ class DmsfController < ApplicationController
         format.html do
           # Warn about searching in sub-folders
           if @folder && params['set_filter'].present? && params['f'].present?
-            flash[:warning] = l(:notice_search_in_subfolders)
+            flash.now[:warning] = l(:notice_search_in_subfolders)
           end
           @dmsf_count = @query.dmsf_count
           @dmsf_pages = Paginator.new @dmsf_count, per_page_option, params['page']
